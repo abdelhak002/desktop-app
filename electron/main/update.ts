eb0095ccc5816da8e515f80ed9d026fe40ed9c79
Darbeida Abdelhak <darbeida.abdelhak@gmail.com>
2023-08-19 18:06:05 +0100

    feat: finsh working on auto-update

--- electron/main/update.ts
+++ electron/main/update.ts
@@ -1,9 +1,58 @@
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, dialog } from 'electron'
 import {
   type ProgressInfo,
   type UpdateDownloadedEvent,
-  autoUpdater
+  autoUpdater,
+  UpdateInfo
 } from 'electron-updater'
+import log from 'electron-log';
+
+  
+// auto update functions section
+export async function autoUpdate() {
+  await autoUpdater.checkForUpdates();
+  startHandlingUpdateProgress();
+}
+
+function startHandlingUpdateProgress() {
+  autoUpdater.on('error', (e) => {
+    log.error('@error@\n', e);
+    dialog.showErrorBox(
+      'Error: ',
+      e == null ? 'unknown' : (e.stack || e).toString()
+    );
+  });
+  autoUpdater.on('update-downloaded', async (info: UpdateInfo) => {
+    log.info('@update-downloaded@\n', info);
+    await handleUpdateDownloaded();
+    // Remove all event listeners
+    autoUpdater.removeAllListeners();
+  });
+}
+
+async function handleUpdateDownloaded() {
+  if ((await askRestartAndInstall()) === InstallDialogResult.NotNow) {
+    return;
+  }
+  setTimeout(() => autoUpdater.quitAndInstall(true, true), 1);
+}
+
+enum InstallDialogResult {
+  InstallAndRestart = 0,
+  NotNow = 1,
+}
+async function askRestartAndInstall(): Promise<InstallDialogResult> {
+  const installDialogResult = await dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Install and restart', 'Later'],
+    message: `A new version of ${app.name} has been downloaded.`,
+    detail: 'It will be installed the next time you restart the application.',
+    defaultId: InstallDialogResult.InstallAndRestart,
+    cancelId: InstallDialogResult.NotNow,
+  });
+  return installDialogResult.response;
+  }
+
 
 export function update(win: Electron.BrowserWindow) {
 
@@ -13,9 +62,21 @@ export function update(win: Electron.BrowserWindow) {
   autoUpdater.allowDowngrade = false
 
   // start check
-  autoUpdater.on('checking-for-update', function () { })
+  autoUpdater.on('checking-for-update', async function () {
+    if (!app.isPackaged) {
+      const error = new Error('The update feature is only available after the package.')
+      return { message: error.message, error }
+    }
+
+    try {
+      console.log('check-update')
+      return await autoUpdater.checkForUpdatesAndNotify()
+    } catch (error) {
+      return { message: 'Network error', error }
+    } })
   // update available
   autoUpdater.on('update-available', (arg) => {
+    console.log('update-available', arg)
     win.webContents.send('update-can-available', { update: true, version: app.getVersion(), newVersion: arg?.version })
   })
   // update not available
@@ -31,6 +92,7 @@ export function update(win: Electron.BrowserWindow) {
     }
 
     try {
+      console.log('check-update')
       return await autoUpdater.checkForUpdatesAndNotify()
     } catch (error) {
       return { message: 'Network error', error }
@@ -46,6 +108,7 @@ export function update(win: Electron.BrowserWindow) {
           event.sender.send('update-error', { message: error.message, error })
         } else {
           // feedback update progress message
+          console.log('start-download', progressInfo)
           event.sender.send('download-progress', progressInfo)
         }
       },
@@ -58,7 +121,8 @@ export function update(win: Electron.BrowserWindow) {
 
   // Install now
   ipcMain.handle('quit-and-install', () => {
-    autoUpdater.quitAndInstall(true, true)
+    console.log('quit-and-install')
+    autoUpdater.quitAndInstall()
   })
 }
 
@@ -66,6 +130,7 @@ function startDownload(
   callback: (error: Error | null, info: ProgressInfo | null) => void,
   complete: (event: UpdateDownloadedEvent) => void,
 ) {
+  console.log('start-downloading')
   autoUpdater.on('download-progress', info => callback(null, info))
   autoUpdater.on('error', error => callback(error, null))
   autoUpdater.on('update-downloaded', complete)
